9fdd0ffe31372b390bd375e65d62b34e7ab70f8e
aelmer <a-elmer@onu.edu>
2016-05-18 12:05:42 -0400

    Android 5 Programming By Example TimeKeeper App. Uses fragments to utilize TextClock and AnalogClock fragments. Added Menu items and learned how to change appearance of menu. Basic clock, analog or digital - further project on own to use as alarm clock (multiple snooze times) and timer (multiple timers running at once)

--- app/src/main/java/ae/timekeeper/MainActivity.java
+++ app/src/main/java/ae/timekeeper/MainActivity.java
@@ -1,27 +1,46 @@
 package ae.timekeeper;
 
-import android.app.Activity;
+
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button analogButton, digitalButton;
+    private Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("Clock");
+        //getActionBar().setTitle("Clock"); Works with Material theme not ApCompat
+        myToolbar.setTitleTextColor(Color.WHITE);
+        myToolbar.setSubtitle("tells the time");
+        myToolbar.setSubtitleTextColor(Color.WHITE);
+        myToolbar.setLogo(R.drawable.access_time_36dp);
+
         analogButton = (Button) findViewById(R.id.button_analog);
         analogButton.setOnClickListener(this);
 
         digitalButton = (Button) findViewById(R.id.button_digital);
         digitalButton.setOnClickListener(this);
+
     }
 
     @Override
@@ -36,6 +55,31 @@ public class MainActivity extends Activity implements View.OnClickListener{
         replaceFragment(fragment);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Fragment fragment;
+
+        int id = item.getItemId();
+        switch(id){
+            case R.id.menu_date:
+                startActivity(new Intent(Settings.ACTION_DATE_SETTINGS));
+                break;
+            case R.id.menu_location:
+                startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
+                break;
+            case R.id.menu_sleep:
+                startActivity(new Intent(Settings.ACTION_SOUND_SETTINGS));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void replaceFragment(Fragment fragment){
 
         FragmentManager manager = getFragmentManager();
@@ -44,6 +88,6 @@ public class MainActivity extends Activity implements View.OnClickListener{
         transaction.replace(R.id.fragment_container, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
-        //manager.executePendingTransactions();
+
     }
 }
---
+++ app/src/main/res/menu.java
@@ -0,0 +1,7 @@
+import ae.timekeeper.MainActivity;
+
+/**
+ * Created by amy on 5/13/2016.
+ */
+public class menu extends MainActivity {
+}
